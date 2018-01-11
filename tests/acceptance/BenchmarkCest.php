b5a1041d9f57d9a32221fd9418f679c450f7d248
Yngve Høiseth <yngve@hoiseth.net>
2018-01-11 15:14:42 +0100

    Fix test when using Turbolinks

--- tests/acceptance/BenchmarkCest.php
+++ tests/acceptance/BenchmarkCest.php
@@ -13,12 +13,29 @@ class BenchmarkCest
 
     public function benchmark(AcceptanceTester $I)
     {
-        $I->amOnPage('/');
+//        $I->amOnPage('/');
 
         for ($i = 0; $i < 10; $i++) {
-            $I->click('Browse application');
-            $I->click('Pellentesque vitae velit ex');
-            $I->click('Symfony Demo');
+            $I->amOnPage('/');
+//            $I->wait(1);
+//            $I->waitForElementVisible('a[href="/en/blog/"]');
+            $I->waitForElement('a[href="/en/blog/"]');
+//            $I->click('Browse application');
+            $I->click('a[href="/en/blog/"]');
+
+
+//            $I->wait(1);
+//            $I->waitForElementVisible('a[href="/en/blog/posts/pellentesque-vitae-velit-ex"]');
+            $I->waitForElement('a[href="/en/blog/posts/pellentesque-vitae-velit-ex"]');
+//            $I->click('Pellentesque vitae velit ex');
+            $I->click('a[href="/en/blog/posts/pellentesque-vitae-velit-ex"]');
+
+
+//            $I->wait(1);
+//            $I->waitForElementVisible('a[href="/"]');
+            $I->waitForElement('a[href="/"]');
+//            $I->click('Symfony Demo');
+            $I->click('a[href="/"]');
         }
     }
 }
